5dbb2f17c6308ace0770d9f050cc07928c4e341c
Vitor Menoli <vitoragario@gmail.com>
2025-04-22 02:27:46 -0300

    fix: github-logo path

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,16 +1,18 @@
+import githubLogo from '../assets/github-logo.png';
+
 function PageTitle() {
-return (
+  return (
     <div className="flex flex-col sm:flex-row justify-center items-center gap-2 mt-10">
-            <img
-                className="w-20 h-20"
-                src="src/assets/github-logo.png"
-                alt="GitHub Logo"
-            />
-            <h1 className="text-center text-5xl">
-                Perfil <span className="font-bold">GitHub</span>
-            </h1>
+      <img
+        className="w-20 h-20"
+        src={githubLogo}
+        alt="GitHub Logo"
+      />
+      <h1 className="text-center text-5xl">
+        Perfil <span className="font-bold">GitHub</span>
+      </h1>
     </div>
-);
+  );
 }
 
 export default PageTitle;
